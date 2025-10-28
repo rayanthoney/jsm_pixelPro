2d263a1153de2380ecdee6f51a0147eccf404970
RayAnthoney <rmaxey911@gmail.com>
2025-10-28 06:47:10 -0600

    Created a ProductViewer on branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Hero from './components/Hero.jsx'
 import NavBar from './components/NavBar.jsx'
+import ProductViewer from './components/ProductViewer.jsx'
 
 const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
+      <ProductViewer />
     </main>
   )
 }
